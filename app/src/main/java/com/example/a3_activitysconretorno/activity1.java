e6e90250edf8ee31daeba7f7993323340bbd2393
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-15 10:40:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a3_activitysconretorno/activity1.java
@@ -0,0 +1,15 @@
+package com.example.a3_activitysconretorno;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity1 extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle saved)
+    {
+        super.onCreate(saved);
+        setContentView(R.layout.activity1);
+    }
+}
